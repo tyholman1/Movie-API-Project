c81851a315f781469f4a486305ac5582ef723a94
ty Holman <tyholman1@gmail.com>
2023-08-23 22:03:16 -0400

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios  from 'axios';
 
 function App() {
+
+  const [disneyChar, setDisneyChar ] = useState([])
+
+  const getMovie = async () => {
+    try {
+      const response = await axios.get("https://api.disneyapi.dev/character?name=Mickey%20Mouse")
+      const data = response.data
+      setDisneyChar(data.data[1].films)
+      // console.log(data.data[1].films)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() =>{
+    getMovie()
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="App">
+        <h1>{getMovie}</h1>
+      </div>
+    );
+  }
 
 export default App;
